acd9776caf500d4fc6372b951b1cc55564a7eebc
Joyce Thomas <122466534+joycethomas@users.noreply.github.com>
2026-06-26 23:29:57 -0400

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        subs, curr = [], []
+
+        def helper(i):
+            if i >= len(nums):
+                subs.append(curr.copy())
+                return 
+            
+
+            curr.append(nums[i])
+            helper(i + 1)
+            curr.pop()
+            helper(i + 1)
+
+        helper(0)
+        return subs
