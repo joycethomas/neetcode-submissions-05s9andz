d2ba704cc06bba851e7462111edb97cf2003a772
Joyce Thomas <122466534+joycethomas@users.noreply.github.com>
2026-07-23 23:31:11 -0400

    Add: path-sum - submission-1

---
+++ Data Structures & Algorithms/path-sum/submission-1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+
+        def dfs(root, total):
+            if not root:
+                return False
+            total += root.val
+            if not root.right and not root.left and total == targetSum:
+                #return True
+                return total == targetSum
+            
+            return dfs(root.right, total) or dfs(root.left, total)
+            
+
+        return dfs(root, 0)
